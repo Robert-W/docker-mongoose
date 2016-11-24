c0dcc4ae74cbf8e9b9ff09c11cce81d99aa2d3a3
Robert-W <RWinter85@gmail.com>
2016-11-24 02:14:44 -0500

    Updated css, added form around login elements

--- src/app/login/client/components/Login.js
+++ src/app/login/client/components/Login.js
@@ -2,33 +2,43 @@ import React, {Component} from 'react';
 import 'login/css/login.scss';
 
 export default class Login extends Component {
+
+  signIn = () => {
+    console.log('signin');
+  };
+
+  signUp = () => {
+    console.log('signup');
+  };
+
   render () {
     return (
-      <div className='login__container shadow flex'>
-        <h2 className='login__form-title'>Login</h2>
-        <input type='text'
-          ref='username'
-          name='username'
-          placeholder='Username'
-          className='login__input no-outline' />
-        <input type='password'
-          ref='password'
-          name='password'
-          placeholder='Password'
-          className='login__input no-outline' />
-        <div className='login__actions flex'>
-          <button type='submit' className='login__button button-theme no-outline'>
-            Sign In
-          </button>
-          <button type='button' className='login__button button-theme no-outline'>
-            Sign Up
-          </button>
-        </div>
-        {/* <form method='post' action='auth/signin'>
-          <input type='text' name='username' />
-          <input type='password' name='password' />
-          <input type='submit' value='Sign In' />
-        </form> */}
+      <div className='login__container shadow'>
+        <form method='post' action='auth/signin' className='login__form flex'>
+          <h2 className='login__form-title'>Login</h2>
+          <input type='text'
+            ref='username'
+            name='username'
+            placeholder='Username'
+            className='login__form-input no-outline' />
+          <input type='password'
+            ref='password'
+            name='password'
+            placeholder='Password'
+            className='login__form-input no-outline' />
+          <div className='login__form-actions flex'>
+            <button type='submit'
+              onClick={this.signIn}
+              className='login__form-buttons button-theme no-outline'>
+              Sign In
+            </button>
+            <button type='button'
+              onClick={this.signUp}
+              className='login__form-buttons button-theme no-outline'>
+              Sign Up
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
