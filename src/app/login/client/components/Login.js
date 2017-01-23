e2d82453153ba631bef53ad72b9bd75778167dc8
Robert <robert-w@Roberts-MacBook-Pro.local>
2017-01-22 23:24:45 -0500

    Added some basic shared components Updated some eslint rules

--- src/app/login/client/components/Login.js
+++ src/app/login/client/components/Login.js
@@ -1,3 +1,4 @@
+import Page from 'shared/components/Page';
 import React, {Component} from 'react';
 import 'login/css/login.scss';
 
@@ -13,33 +14,35 @@ export default class Login extends Component {
 
   render () {
     return (
-      <div className='login__container shadow'>
-        <form method='post' action='auth/signin' className='login__form flex'>
-          <h2 className='login__form-title'>Login</h2>
-          <input type='text'
-            ref='username'
-            name='username'
-            placeholder='Username'
-            className='login__form-input no-outline' />
-          <input type='password'
-            ref='password'
-            name='password'
-            placeholder='Password'
-            className='login__form-input no-outline' />
-          <div className='login__form-actions flex'>
-            <button type='submit'
-              onClick={this.signIn}
-              className='login__form-buttons button-theme no-outline'>
-              Sign In
-            </button>
-            <button type='button'
-              onClick={this.signUp}
-              className='login__form-buttons button-theme no-outline'>
-              Sign Up
-            </button>
-          </div>
-        </form>
-      </div>
+      <Page>
+        <div className='login__container shadow'>
+          <form method='post' action='auth/signin' className='login__form flex'>
+            <h2 className='login__form-title'>Login</h2>
+            <input type='text'
+              ref='username'
+              name='username'
+              placeholder='Username'
+              className='login__form-input no-outline' />
+            <input type='password'
+              ref='password'
+              name='password'
+              placeholder='Password'
+              className='login__form-input no-outline' />
+            <div className='login__form-actions flex'>
+              <button type='submit'
+                onClick={this.signIn}
+                className='login__form-buttons button-theme no-outline'>
+                Sign In
+              </button>
+              <button type='button'
+                onClick={this.signUp}
+                className='login__form-buttons button-theme no-outline'>
+                Sign Up
+              </button>
+            </div>
+          </form>
+        </div>
+      </Page>
     );
   }
 }
